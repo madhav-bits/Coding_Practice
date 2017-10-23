eb0990d45048de697b643033d10eea92fb432f6b
Madhav <kasamsaimadhavk@gmail.com>
2017-10-23 01:00:15 -0400

    Added a comment in the code.

--- leetFindDupNum1.cpp
+++ leetFindDupNum1.cpp
@@ -2,7 +2,6 @@
 Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
 
 Note:
-You must not modify the array (assume the array is read only).
 You must use only constant, O(1) extra space.
 Your runtime complexity should be less than O(n2).
 There is only one duplicate number in the array, but it could be repeated more than once.
@@ -16,7 +15,7 @@ public:
     int findDuplicate(vector<int>& nums) {
         if(nums.size()==0)
             return -1;
-        sort(nums.begin(), nums.end());// Sort the array.
+        sort(nums.begin(), nums.end());// Sort the array. Complexity is O(nlogn) using quick sort. USes O(1) space by the way.
         for(int i=0;i+1<nums.size();i++){
             if(nums[i]==nums[i+1])// First repitition is the desired answer.
                 return nums[i];
