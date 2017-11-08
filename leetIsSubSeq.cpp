16fefbb0673d6f9c9626517e4fb8cb358660da23
Madhav <kasamsaimadhavk@gmail.com>
2017-11-07 20:41:16 -0500

    Added leetIsSubSeq.cpp file.

---
+++ leetIsSubSeq.cpp
@@ -0,0 +1,45 @@
+/*
+//****************************************************392. Is Subsequence************************************************
+Given a string s and a string t, check if s is subsequence of t.
+
+You may assume that there is only lower case English letters in both s and t. t is potentially 
+a very long (length ~= 500,000) string, and s is a short string (<=100).
+
+A subsequence of a string is a new string which is formed from the original string by deleting some 
+(can be none) of the characters without disturbing the relative positions of the remaining characters. 
+
+Example 1:
+s = "abc", t = "ahbgdc"
+
+Return true.
+
+Example 2:
+s = "axc", t = "ahbgdc"
+
+Return false.
+
+Follow up:------------DONE.
+If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you want to check one by one to see if 
+T has its subsequence. In this scenario, how would you change your code?
+
+Credits:
+Special thanks to @pbrother for adding this problem and creating all test cases.
+//*****************************************************THIS IS LEET ACCEPTED CODE.*********************************************
+
+*/
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if(s=="" && t=="")// When both are NULL.
+            return true;
+        int i=0;
+        for(int j=0;j<t.length();j++){
+            if(t[j]==s[i])// When an elem. in S is also in T.
+                i++;
+            
+            if(i==s.length())
+                return true;
+        }
+        return false;
+    }
+};
