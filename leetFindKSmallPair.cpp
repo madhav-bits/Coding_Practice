84a24031792bda49e8b090ba18faaf53f3f5ff45
Madhav <kasamsaimadhavk@gmail.com>
2017-12-10 16:24:19 -0500

    Added leetFindKSmallPair.cpp file.

---
+++ leetFindKSmallPair.cpp
@@ -0,0 +1,81 @@
+/*
+
+//***************************************************373. Find K Pairs with Smallest Sums.**********************************************
+
+You are given two integer arrays nums1 and nums2 sorted in ascending order and an integer k.
+
+Define a pair (u,v) which consists of one element from the first array and one element from the second array.
+
+Find the k pairs (u1,v1),(u2,v2) ...(uk,vk) with the smallest sums.
+
+Example 1:
+Given nums1 = [1,7,11], nums2 = [2,4,6],  k = 3
+
+Return: [1,2],[1,4],[1,6]
+
+The first 3 pairs are returned from the sequence:
+[1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+Example 2:
+Given nums1 = [1,1,2], nums2 = [1,2,3],  k = 2
+
+Return: [1,1],[1,1]
+
+The first 2 pairs are returned from the sequence:
+[1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
+Example 3:
+Given nums1 = [1,2], nums2 = [3],  k = 3 
+
+Return: [1,3],[2,3]
+
+All possible pairs are returned from the sequence:
+[1,3],[2,3]
+Credits:
+Special thanks to @elmirap and @StefanPochmann for adding this problem and creating all test cases.
+
+
+//Time Complexity: O(k * log(min(n, m, k))), where n is the size of num1, and m is the size of num2.
+// Space Complexity: O(min(n, m, k)).
+
+//********************************************************THIS IS LEET ACCEPTED CODE.***************************************************
+*/
+
+
+//Solution 1:
+// This can be done using Max Heap and iterate through out the loop. It would take O((m*n)logk) time and O(k) space.
+// Need to work on it. 
+ 
+class Solution {
+public:
+    vector<pair<int, int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        vector<pair<int, int>> pairs;
+        if (nums1.size() > nums2.size()) {
+            vector<pair<int, int>> tmp = kSmallestPairs(nums2, nums1, k);
+            for (const auto& pair : tmp) {
+                pairs.emplace_back(pair.second, pair.first);
+            }
+            return pairs;
+        }
+
+        using P = pair<int, pair<int, int>>;
+        priority_queue<P, vector<P>, greater<P>> q;
+        auto push = [&nums1, &nums2, &q](int i, int j) {        //What does this function actually do?
+            cout<<" inside push with i is: "<<i<<" and j is: "<<j<<endl;
+            if (i < nums1.size() && j < nums2.size()) {
+                q.emplace(nums1[i] + nums2[j], make_pair(i, j));
+            }
+        };
+
+        push(0, 0);
+        while (!q.empty() && pairs.size() < k) {
+            auto tmp = q.top(); q.pop();
+            int i, j;
+            tie(i, j) = tmp.second;
+            pairs.emplace_back(nums1[i], nums2[j]);
+            push(i, j + 1);             //Then here it will keep on increasing indices in vector2.   
+            if (j == 0) {               //Why is it calling next value in first vector here.
+                push(i + 1, 0);  // at most queue min(m, n) space.
+            }
+        }
+        return pairs; 
+    }
+};
