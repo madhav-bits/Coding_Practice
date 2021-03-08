d52fe46c331ccb8137ef8e6539d0273a77bab2f3
madhav-bits <kasamsaimadhavk@gmail.com>
2021-03-08 03:09:07 -0500

    Added leetSplitArrayLargestSum.cpp file and made name changes in leetCapacityShipPkgDDays.cpp file.

--- leetSplitArrayLargestSum.cpp
+++ leetSplitArrayLargestSum.cpp
@@ -1,55 +1,41 @@
 /*
 *
-//******************************************************1011. Capacity To Ship Packages Within D Days.***********************************************
+//******************************************************410. Split Array Largest Sum.***********************************************
 
-https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
+https://leetcode.com/problems/split-array-largest-sum/
 
 
-A conveyor belt has packages that must be shipped from one port to another within D days.
 
-The ith package on the conveyor belt has a weight of weights[i]. Each day, we load the ship with packages on the conveyor belt (in the order 
-given by weights). We may not load more weight than the maximum weight capacity of the ship.
 
-Return the least weight capacity of the ship that will result in all the packages on the conveyor belt being shipped within D days.
+Given an array nums which consists of non-negative integers and an integer m, you can split the array into m non-empty continuous subarrays.
+
+Write an algorithm to minimize the largest sum among these m subarrays.
 
  
 
 Example 1:
 
-Input: weights = [1,2,3,4,5,6,7,8,9,10], D = 5
-Output: 15
-Explanation: A ship capacity of 15 is the minimum to ship all the packages in 5 days like this:
-1st day: 1, 2, 3, 4, 5
-2nd day: 6, 7
-3rd day: 8
-4th day: 9
-5th day: 10
-
-Note that the cargo must be shipped in the order given, so using a ship of capacity 14 and splitting the packages into parts 
-like (2, 3, 4, 5), (1, 6, 7), (8), (9), (10) is not allowed.
+Input: nums = [7,2,5,10,8], m = 2
+Output: 18
+Explanation:
+There are four ways to split nums into two subarrays.
+The best way is to split it into [7,2,5] and [10,8],
+where the largest sum among the two subarrays is only 18.
 Example 2:
 
-Input: weights = [3,2,2,4,1,4], D = 3
-Output: 6
-Explanation: A ship capacity of 6 is the minimum to ship all the packages in 3 days like this:
-1st day: 3, 2
-2nd day: 2, 4
-3rd day: 1, 4
+Input: nums = [1,2,3,4,5], m = 2
+Output: 9
 Example 3:
 
-Input: weights = [1,2,3,1,1], D = 4
-Output: 3
-Explanation:
-1st day: 1
-2nd day: 2
-3rd day: 3
-4th day: 1, 1
+Input: nums = [1,4,4], m = 3
+Output: 4
  
 
 Constraints:
 
-1 <= D <= weights.length <= 5 * 104
-1 <= weights[i] <= 500
+1 <= nums.length <= 1000
+0 <= nums[i] <= 106
+1 <= m <= min(50, nums.length)
 
 
 
@@ -84,6 +70,9 @@ Constraints:
 // the low value at the end of the search.
 
 
+// This question is modified version of Question: 1011, Capacity to Ship Packages Within D days. So, I had used the exact soluion here.
+// Here is the link to 1011 question.
+// https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
 
 
 
@@ -93,7 +82,7 @@ Constraints:
 
 class Solution {
 public:
-    int shipWithinDays(vector<int>& v, int D) {
+    int shipWithinDays(vector<int>& v, int m) {
         int low=v[0], high=v[0];
         for(int num:v) low=max(low, num), high+=num;
         while(low<high){
@@ -109,11 +98,10 @@ public:
             }
             cnt++;
             // cout<<"mid: "<<mid<<" and cnt: "<<cnt<<endl;
-            if(cnt>D) low=mid+1;											// If cnt>D, we inc. the cap to dec. the cnt(#days).
+            if(cnt>m) low=mid+1;											// If cnt>D, we inc. the cap to dec. the cnt(#days).
             else high=mid;													// If cnt<=D, we search in lower half for least possible day cap.
         }
         return low;															// Returning the least possible day cap.
     }
 };
-
 
