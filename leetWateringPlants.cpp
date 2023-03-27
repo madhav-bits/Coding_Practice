973b75a2f567150c49fb2d3ecc0768f5a577afa0
madhav-bits <kasamsaimadhavk@gmail.com>
2023-03-26 23:55:19 -0700

    Made Time, Space complexity related changes for leetWateringPlants.cpp file.

--- leetWateringPlants.cpp
+++ leetWateringPlants.cpp
@@ -21,8 +21,8 @@ https://leetcode.com/problems/watering-plants/
 
 
 
-// Time Complexity: O(n!).  
-// Space Complexity: O(n*n!).
+// Time Complexity: O(n).  
+// Space Complexity: O(1).
 
 //********************************************************THIS IS LEET ACCEPTED CODE.***************************************************
 
@@ -30,8 +30,8 @@ https://leetcode.com/problems/watering-plants/
 
 //************************************************************Solution 1:************************************************************
 //*****************************************************THIS IS LEET ACCEPTED CODE.***********************************************
-// Time Complexity: O(n!).
-// Space Complexity: O(n*n!).	
+// Time Complexity: O(n).
+// Space Complexity: O(1).	
 // This algorithm is iteration based. Here, when we are at index 'i' in the iteration, we are physically at index 'i-1', if the curr. water > water needed at index 'i', we remove the water from curr.
 // water and move to curr. index, if curr. water < water needed at index 'i', we travel to well( index: -1) and return to index 'i-1', so we add 2*i for the distance covered and add +1 for moving
 // to curr. index after watering the plant. We return the total distance/steps made at the end of iter.
