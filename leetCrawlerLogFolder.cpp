53d96b6fd829e92a8619561097b90d77c9b0fc69
madhav-bits <kasamsaimadhavk@gmail.com>
2023-04-05 23:56:02 -0700

    Made some description changes to leetCrawlerLogFolder.cpp file.

--- leetCrawlerLogFolder.cpp
+++ leetCrawlerLogFolder.cpp
@@ -29,6 +29,7 @@ Worked with the test cases provided with question.
 // This algorithm is comparison based. Here, for every log we compare them with possible logs, we keep track of depth of the curr. folder
 // from the main folder by the var.(steps). If we encounter "../" we dec. the count, if we encounter any string other than "./" we inc.
 // the count. At the end,we return the cnt, as these are the steps we have to take to go to main folder.
+// While dec. the count, we also ensure this is >=0, as the move to parent oper. won't work when in main directory.
 
 
 
@@ -39,8 +40,8 @@ Worked with the test cases provided with question.
 class Solution {
 public:
     int minOperations(vector<string>& logs) {
-        int steps=0;
-        for(auto str: logs){
+        int steps=0;														// Tracks the recursion depth of the directory we are in.
+        for(auto&str: logs){
             if(str=="../") steps=max(0, steps-1);							// Dec. cnt as we go to parent folder.
             else if(str!="./") steps++;										// We go to child folder.
         }
